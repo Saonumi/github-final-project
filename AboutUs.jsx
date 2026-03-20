1dfe13ceba593f266d65d1cd0fdd6de3037b8774
Saonumi <sang3172005@gmail.com>
2026-03-20 09:08:41 +0700

    Add AboutUs component for nursery information

---
+++ AboutUs.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const AboutUs = () => {
+  return (
+    <div className="about-us-container">
+      <h1>About Paradise Nursery</h1>
+      <p>Welcome to Paradise Nursery, where green dreams come true. Our mission is to bring the beauty of nature into every home by providing high-quality, air-purifying houseplants.</p>
+    </div>
+  );
+};
+export default AboutUs;
